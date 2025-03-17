7bcb7610dcf21dd1581ad08ff6a65c7e56d769c2
Víctor Navarro <vicnala@gmail.com>
2025-03-17 19:21:25 +0100

    Logout at the end of OTP verification process

--- src/components/HomeIndex/User.tsx
+++ src/components/HomeIndex/User.tsx
@@ -14,6 +14,7 @@ import NotConnected from '../NotConnected';
 import ButtonSpinner from '@/components/UI/ButtonSpinner';
 import FormSaveButton from '@/components/UI/FormSaveButton';
 import { CircleCheck } from 'lucide-react';
+import { logout } from '@/actions/login';
 
 export default function User(
   { locale }: Readonly<{ locale: string }>
@@ -124,6 +125,7 @@ export default function User(
       }
       if (otpRef && otpRef.current) otpRef.current.value = '';
       setReloadUser(true);
+      await logout();
     } catch (error) {
       console.log('sendUserOTPVerification', error)
     } finally {
