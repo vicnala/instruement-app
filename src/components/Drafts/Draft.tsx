e2ba8087644336ec7bf3f4fb7ea2f2e9afcc0aff
Víctor Navarro <vicnala@gmail.com>
2025-05-16 08:36:38 +0200

    Show Skeleton while instrument loading

--- src/components/Drafts/Draft.tsx
+++ src/components/Drafts/Draft.tsx
@@ -44,36 +44,40 @@ export default function Draft(
   }, [instrumentId, locale, api_key, isLoading, instrument]);
 
   return (
-    instrument && !instrument.asset_id ?
     <div
       className="cursor-pointer transition-all hover:shadow-lg || flex flex-col w-full justify-stretch min-h-[200px] || overflow-hidden bg-it-25 border border-it-100 rounded-lg"
       onClick={() => router.push(`/drafts/${instrumentId}`)}
     >
       <div className="relative w-full aspect-square bg-white/[.04]">
         {
-          instrument.queue_id ?
-          <div className="absolute top-2 left-2 p-2 bg-black/50 rounded-full hover:bg-black/70 transition-all">
-            {t("registering")}
-          </div> :
-          <div className="absolute top-2 right-2 p-2 bg-black/50 rounded-full hover:bg-black/70 transition-all">
-            <IconEdit 
-              width="20"
-              height="20"
-              className="text-white"
-            />
-          </div>
-        }
-        { image ? 
-          <Image
-            src={image.file_url}
-            width={500}
-            height={500}
-            alt={image.description}
-            className="object-cover w-full h-full"
-          /> :
-          <div className="w-full h-full rounded-lg">
-            <Skeleton width="100%" height="100%" />
-          </div>
+          isLoading ?
+          <Skeleton width="100%" height="100%" /> :
+          <>
+            {
+              instrument && instrument.queue_id ?
+              <div className="absolute top-2 left-2 p-2 bg-black/50 rounded-full hover:bg-black/70 transition-all">
+                {t("registering")}
+              </div> :
+              <div className="absolute top-2 right-2 p-2 bg-black/50 rounded-full hover:bg-black/70 transition-all">
+                <IconEdit 
+                  width="20"
+                  height="20"
+                  className="text-white"
+                />
+              </div>
+            }
+            { image ? 
+              <Image
+                src={image.file_url}
+                width={500}
+                height={500}
+                alt={image.description}
+                className="object-cover w-full h-full"
+              /> :
+              <div className="w-full h-full rounded-lg">
+              </div>
+            }
+          </>
         }
       </div>
       <div className="flex items-center justify-between flex-1 w-full px-3">
@@ -86,6 +90,6 @@ export default function Draft(
           </p>
         </div>
       </div>
-    </div> : <></>
+    </div>
   );
 }
