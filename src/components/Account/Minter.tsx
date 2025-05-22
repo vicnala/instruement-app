94f31d76b49c8e7afc3cc2ea4f4f5be9745e6309
unfresco <ortiz.arandes@gmail.com>
2025-05-22 11:35:09 +0200

    Use Image component

--- src/components/Account/Minter.tsx
+++ src/components/Account/Minter.tsx
@@ -32,10 +32,13 @@ export default function Minter(
             {/* Cover Image Section */}
             <div className="relative w-full h-60 mb-16">
                 <div className="w-full h-full overflow-hidden rounded-[15px]">
-                    <img
+                    <Image
                         src={minter?.cover_image?.url || "https://static.instruement.com/web/img/default-cover.jpg"}
                         alt={minter?.business_name || "Cover"}
                         className="w-full h-full object-cover"
+                        width={1000}
+                        height={250}
+                        priority
                     />
                 </div>
 
@@ -43,10 +46,13 @@ export default function Minter(
                 {minter?.profile_photo && (
                     <div className="absolute -bottom-16 left-24 transform -translate-x-1/2">
                         <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white shadow-lg">
-                            <img
+                            <Image
                                 src={minter.profile_photo.url}
                                 alt={`${minter.first_name} ${minter.last_name}`}
                                 className="w-full h-full object-cover"
+                                width={150}
+                                height={150}
+                                priority
                             />
                         </div>
                     </div>
